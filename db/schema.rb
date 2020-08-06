833ce60ffdb6fe52bd394d4eac74702f04c9712c
Bryce B <bryce.boddy@gmail.com>
2020-08-06 08:25:19 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
